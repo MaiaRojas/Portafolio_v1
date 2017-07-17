7cbaa0e51b1b4c452ec71680f7e5d38f795642da
Maia <maia.rt.46@gmail.com>
2017-07-17 16:34:39 -0500

    CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,34 +6,34 @@ $(document).ready(function(){
       outDuration: 300,
    }
    );
- $('#modal2').modal({
-    dismissible: false, // Modal can be dismissed by clicking outside of the modal
-    inDuration: 500, // Transition in duration
-    outDuration: 200, // Transition out duration
-   });
-$('#modal3').modal({
-   dismissible: false, // Modal can be dismissed by clicking outside of the modal
-   inDuration: 500, // Transition in duration
-   outDuration: 200, // Transition out duration
- });
- $('#modal4').modal({
-    dismissible: false, // Modal can be dismissed by clicking outside of the modal
-    inDuration: 500, // Transition in duration
-    outDuration: 200, // Transition out duration
-  });
-  $('#modal5').modal({
+  $('#modal2').modal({
+      dismissible: false, // Modal can be dismissed by clicking outside of the modal
+      inDuration: 500, // Transition in duration
+      outDuration: 200, // Transition out duration
+     });
+  $('#modal3').modal({
      dismissible: false, // Modal can be dismissed by clicking outside of the modal
      inDuration: 500, // Transition in duration
      outDuration: 200, // Transition out duration
    });
-   $('#modal6').modal({
+  $('#modal4').modal({
       dismissible: false, // Modal can be dismissed by clicking outside of the modal
       inDuration: 500, // Transition in duration
       outDuration: 200, // Transition out duration
     });
+  $('#modal5').modal({
+       dismissible: false, // Modal can be dismissed by clicking outside of the modal
+       inDuration: 500, // Transition in duration
+       outDuration: 200, // Transition out duration
+     });
+  $('#modal6').modal({
+        dismissible: false, // Modal can be dismissed by clicking outside of the modal
+        inDuration: 500, // Transition in duration
+        outDuration: 200, // Transition out duration
+      });
 
-   $('.parallax').parallax();
-   $(".menu-collapsed").click(function() {
+  $('.parallax').parallax();
+  $(".menu-collapsed").click(function() {
      $(this).toggleClass("menu-expanded");
      $('.redes').toggleClass("hide");
    });
